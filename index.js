3b8c750d5a7e4b2b21bd6d08b7f28200f39211f4
FurryNashwaAspirna <furryaspirnaa11@gmail.com>
2019-09-25 07:33:56 +0700

    latihanExpress

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const port = 8000;
 
 app.get('/', (req, res)=> {
     res.send('Halo Aku Express!!');
+});
+
+app.delete('/delete', (req, res)=> {
+    res.send('Delete');
+});
+
+app.post('/register', (req, res)=> {
+    res.send('Request for register');
 })
 
 app.listen(port, ()=> console.log(`App listen on port ${port}`))
