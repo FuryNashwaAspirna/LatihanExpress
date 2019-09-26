b2882b5ef02fe70bdd090b9672bd58d35bb1fdcd
FurryNashwaAspirna <furryaspirnaa11@gmail.com>
2019-09-26 07:17:55 +0700

    lanjutan kmrn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res)=> {
     res.send('Halo Aku Express!!');
 });
 
-app.delete('/delete', (req, res)=> {
+app.get('/login', (req, res)=> {
     res.send('Delete');
 });
 
@@ -14,4 +14,11 @@ app.post('/register', (req, res)=> {
     res.send('Request for register');
 })
 
+app.pud('/instal', (req, res) => {
+    res.send('Berhasil di install!')
+});
+
+app.pud('/delete', (req, res) => {
+    res.send('Berhasil di install!')
+});
 app.listen(port, ()=> console.log(`App listen on port ${port}`))
